cdff5329ccd94ab2e1cbf01bf0be9fb77c1b8c0c
Rômulo Quadros <rbersagui@gmail.com>
2015-09-25 13:08:00 -0300

    Initial commit
    Initial commit

---
+++ src/main/java/com/lazyboy/Main.java
@@ -0,0 +1,12 @@
+package com.lazyboy;
+
+import com.sun.codemodel.JCodeModel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JCodeModel codeModel = new JCodeModel();
+		codeModel._package("");
+	}
+
+}
